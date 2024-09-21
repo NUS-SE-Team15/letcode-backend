c4bab420b3a17d332e69fa168d2204bae39b27a1
Demonshaha <572819396@qq.com>
2024-09-22 01:06:59 +0800

    letcode-backend initialisation

---
+++ src/main/java/com/team15/letcode/model/dto/postfavour/PostFavourQueryRequest.java
@@ -0,0 +1,30 @@
+package com.team15.letcode.model.dto.postfavour;
+
+import com.team15.letcode.common.PageRequest;
+import com.team15.letcode.model.dto.post.PostQueryRequest;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 帖子收藏查询请求
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PostFavourQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * 帖子查询请求
+     */
+    private PostQueryRequest postQueryRequest;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
