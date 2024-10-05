32acd82471aa933201579b981cea2e56ac46efc2
Agelessbaby <205902584@qq.com>
2024-10-05 14:23:32 +0800

    increase waiting time to 40

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,6 +1,6 @@
 # validate_service.sh
 echo "Waiting for Spring application to start..."
-sleep 20  # 等待 20 秒，确保 Spring 完全启动
+sleep 40  # 等待 20 秒，确保 Spring 完全启动
 
 if curl -f http://localhost:8101/api/health; then
   echo "Application is running."
