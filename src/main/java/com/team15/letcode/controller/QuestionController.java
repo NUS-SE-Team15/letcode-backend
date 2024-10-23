a7c77bf332f140eb9863f4b7cd9c1449c89a8180
whqwq <1776678487@qq.com>
2024-10-23 14:56:33 +0800

    merge question and questionSubmit

--- src/main/java/com/team15/letcode/controller/QuestionController.java
+++ src/main/java/com/team15/letcode/controller/QuestionController.java
@@ -11,11 +11,16 @@ import com.team15.letcode.constant.UserConstant;
 import com.team15.letcode.exception.BusinessException;
 import com.team15.letcode.exception.ThrowUtils;
 import com.team15.letcode.model.dto.question.*;
+import com.team15.letcode.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.team15.letcode.model.dto.questionsubmit.QuestionSubmitQueryRequest;
 import com.team15.letcode.model.dto.user.UserQueryRequest;
 import com.team15.letcode.model.entity.Question;
+import com.team15.letcode.model.entity.QuestionSubmit;
 import com.team15.letcode.model.entity.User;
+import com.team15.letcode.model.vo.QuestionSubmitVO;
 import com.team15.letcode.model.vo.QuestionVO;
 import com.team15.letcode.service.QuestionService;
+import com.team15.letcode.service.QuestionSubmitService;
 import com.team15.letcode.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.weaver.patterns.TypePatternQuestions;
@@ -35,6 +40,9 @@ public class QuestionController {
 
     @Resource
     private QuestionService questionService;
+
+    @Resource
+    private QuestionSubmitService questionSubmitService;
 
     @Resource
     private UserService userService;
@@ -226,4 +234,29 @@ public class QuestionController {
         return ResultUtils.success(result);
     }
 
+
+    @PostMapping("/question_submit/do")
+    public BaseResponse<Long> doQuestionSubmit(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
+                                               HttpServletRequest request) {
+        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long questionSubmitId = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
+        return ResultUtils.success(questionSubmitId);
+    }
+
+    @PostMapping("/question_submit/list/page")
+    public BaseResponse<Page<QuestionSubmitVO>> listQuestionSubmitByPage(@RequestBody QuestionSubmitQueryRequest questionSubmitQueryRequest,
+                                                                         HttpServletRequest request) {
+        long current = questionSubmitQueryRequest.getCurrent();
+        long size = questionSubmitQueryRequest.getPageSize();
+        // 从数据库中查询原始的题目提交分页信息
+        Page<QuestionSubmit> questionSubmitPage = questionSubmitService.page(new Page<>(current, size),
+                questionSubmitService.getQueryWrapper(questionSubmitQueryRequest));
+        final User loginUser = userService.getLoginUser(request);
+        // 返回脱敏信息
+        return ResultUtils.success(questionSubmitService.getQuestionSubmitVOPage(questionSubmitPage, loginUser));
+    }
 }
